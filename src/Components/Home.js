2173712984000ce67d55e5c1307841069c27f29a
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-07 08:41:30 +0300

    created thted home component

---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <div className="container">
+        <img
+          className="home-image"
+          src="https://m.media-amazon.com/images/I/71cvRGs+pPL._SX3000_.jpg"
+          alt=""
+        />
+        <div className="home-row"></div>
+        <div className="home-row"></div>
+        <div className="home-row"></div>
+        <div className="home-row"></div>
+        <div className="home-row"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
