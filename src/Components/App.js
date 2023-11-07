fd4a1a842affa202797b752a417f20334bcc457d
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-07 08:39:27 +0300

    added home and header

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
