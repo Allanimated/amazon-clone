586efde156277f05dac265323ef3095e87dd8dcb
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-09 13:50:04 +0300

    added Link to img

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Category.css";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Category({ title, image, category }) {
   return (
     <div className="category">
       <h2>{title}</h2>
-      <img src={image} alt="img" />
+      <Link to={`/${category}`}>
+        <img src={image} alt="img" />
+      </Link>
       <div className="category-link">
         <NavLink to={`/${category}`}>Shop Now</NavLink>
       </div>
