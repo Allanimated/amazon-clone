558a7965e10ff19cdad48d1e8a36f5654145baa2
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-09 17:24:32 +0300

    Downgraded to @react17

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 // import the provider
 import { ProductProvider } from "./Context/ProductData";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Router>
     <ProductProvider>
       <App />
     </ProductProvider>
-  </Router>
+  </Router>,
+  document.getElementById("root")
 );
