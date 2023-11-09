e4363434f332f1f687f75c1783ca269be53aa6a3
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-09 10:18:18 +0300

    imported state rovider wrappper

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Components/App";
 import { BrowserRouter as Router } from "react-router-dom";
+// import the provider
+import { ProductProvider } from "./Context/ProductData";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <ProductProvider>
+      <App />
+    </ProductProvider>
   </Router>
 );
