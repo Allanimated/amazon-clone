3078ee931a93d7896fa95794198344b05764aec0
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-10 16:49:55 +0300

    Created SearchResults component

---
+++ src/Components/SearchResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchResults.css";
+
+function SearchResults() {
+  return (
+    <div className="searchResults">
+      <div className="results-container"></div>
+    </div>
+  );
+}
+
+export default SearchResults;
