3bc4e9d9bcdfd160cb8cc3a029a8a66998494dff
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-11 22:34:43 +0300

    Added Search result component

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./SearchResults.css";
+import { useParams } from "react-router-dom";
+import { ProductContext } from "../Context/ProductData";
+import Product from "./Product";
 
 function SearchResults() {
+  const { id } = useParams();
+  const [searchItem, setSearchItem] = useState([]);
+  //use data from context
+  const { cartItems, setCartItems } = useContext(ProductContext);
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((res) => res.json())
+      .then((item) => {
+        setSearchItem([...searchItem, item]);
+      });
+  }, [id]);
+  //handle addToCart request
+  function addToCart(product) {
+    const updatedCartItems = [...cartItems, product];
+    setCartItems(updatedCartItems);
+  }
+
+  const product = searchItem.map((item) => (
+    <Product key={item.title} product={item} addToCart={addToCart} />
+  ));
+
   return (
     <div className="searchResults">
-      <div className="results-container"></div>
+      <div className="results-container">
+        <h1>Results</h1>
+        <hr />
+        <div className="product-row">{product}</div>
+      </div>
     </div>
   );
 }
